a5c80a70f4bb0969e06f80020b1ed635da47670b
jerryc137th <76035472+jerryc137th@users.noreply.github.com>
2021-01-08 17:34:53 +0700

    finished

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,27 +1,29 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
+	char grade;
+	int i=0,a;
+	int count[5]={};
+	cout<<"Please input grade of each student (A-F) or input 0 to exit."<<endl;
 	do{
-		cout << "Student [" << "]:";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
+		i++;
+		cout<<"Student "<<"["<<i<<"]: ";
+		cin>>grade;
+		if(grade =='0')break;
+		(grade=='A')? count[0]++:(grade=='B')? count[1]++:(grade=='C')? count[2]++:(grade=='D')? count[3]++:(grade=='F')? count[4]++:a=i;
+		if(a==i){
+			a--;
+			i--;
+			cout<<"Wrong input. Please input again."<<endl;
+		}
 	}while(true);
-	
-	
-	cout << "In total ? students.";
+	cout << "In total "<<(i-1)<<" students."<<endl;
 	cout << "A = " << count[0] <<", ";
 	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
-	
+	cout << "C = " << count[2] <<", ";
+	cout << "D = " << count[3] <<", ";
+	cout << "F = " << count[4] <<endl;
 	return 0;
 }
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+int main(){
+	string h;
+	float x,i=0,k,j=0,m=0,e;
+	ifstream s;
+	s.open("score.txt");
+	while(getline(s,h)){
+		x = stof(h.c_str());
+		k = pow(x,2);
+		j+=k;
+		m+=x;
+		i++;
+	}
+	s.close();
+	e = sqrt((j/i)-pow((m/i),2));
+	cout << "Number of data = "<<i<<endl;
+    cout << setprecision(3);
+    cout << "Mean = "<<(m/i)<<endl;
+    cout << "Standard deviation = "<<e<<endl;
+	return 0;
+}
